6d7a66938f85f887a80bc66daa639e7abb4df657
Marc Partensky <marc.partensky@gmail.com>
2020-11-15 13:49:21 +0100

    getting my hands on c again

--- Aléatoire2.c
+++ Aléatoire2.c
@@ -3,18 +3,15 @@
 #include <time.h>
 #include <conio.h>
 
-int Random (int _iMin, int _iMax)
-{
-return (_iMin + (rand () % (_iMax-_iMin+1)));
+int Random (int _iMin, int _iMax) {
+    return (_iMin + (rand () % (_iMax-_iMin+1)));
 }
 
-int main (void)
-{
-int iRandom;
-
-srand (time (NULL));
-iRandom = Random (50,100);
-printf ("Random : %d", iRandom);
-getch();
-return 0;
+int main (void) {
+    int iRandom;
+    srand (time (NULL));
+    iRandom = Random (50,100);
+    printf ("Random : %d", iRandom);
+    getch();
+    return 0;
 }
--- Chrono1.c
+++ Chrono1.c
@@ -2,27 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void)
-{
+int main(void) {
     int mystere, nombreentre;
     clock_t temps;
 
     srand(time(NULL));
     mystere=rand()%100+1;
 
-    do{
+    do {
         puts("Quel est le nombre ? ");
         scanf("%d", &nombreentre);
 
-        if(nombreentre>mystere)
+        if (nombreentre>mystere)
             puts("C'est moins !\n");
-        if(nombreentre<mystere)
+        if (nombreentre<mystere)
             puts("C'est plus !\n");
-        if(nombreentre==mystere){
+        if (nombreentre==mystere){
             temps=clock();
             printf("Bravo !\nTu as mis %f secondes a trouver le nombre.\n", (double) temps/CLOCKS_PER_SEC);
         }
-    }while(nombreentre!=mystere);
+    } while (nombreentre != mystere);
 
     return 0;
 }
