c69a117cdc0b4167e21df78290279015332738ea
marc <marc.partensky@gmail.com>
2021-01-13 01:11:16 +0100

    following inverse square root on yt

---
+++ inverse-square-root.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float Q_rsqrt(float number) {
+	long i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y = number;
+	i = * (long * ) &y;
+	i = 0x5f3759df - ( i >> 1 );
+	y = * (float * ) &i;
+	y = y * (threehalfs - (x2 * y * y));
+	y = y * (threehalfs - (x2 * y * y));
+
+	return y;
+}
+
+int main(int argc, char *argv[]) {
+	float i = atof(argv[1]);
+	float o = Q_rsqrt(i);
+	printf("Result: %f", o);
+}
---
+++ test4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+	printf("%d\n", 0xffffff);
+	printf("%d\n", 0xffffff & 1);
+	return 0;
+}
+
